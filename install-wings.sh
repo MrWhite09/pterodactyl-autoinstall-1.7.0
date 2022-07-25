ee2affa9c4a99bee1e68a32d1c5f50fdccd84d7e
MrWhite09 <97874090+MrWhite09@users.noreply.github.com>
2022-07-25 15:50:32 -0400

    Update install-wings.sh

--- install-wings.sh
+++ install-wings.sh
@@ -55,7 +55,7 @@ fi
 
 # download URLs
 WINGS_DL_BASE_URL="https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_"
-GITHUB_BASE_URL="https://raw.githubusercontent.com/vilhelmprytz/pterodactyl-installer/$GITHUB_SOURCE"
+GITHUB_BASE_URL="https://raw.githubusercontent.com/MrWhite09/pterodactyl-autoinstall-1.7.0/$GITHUB_SOURCE"
 
 COLOR_RED='\033[0;31m'
 COLOR_NC='\033[0m'
